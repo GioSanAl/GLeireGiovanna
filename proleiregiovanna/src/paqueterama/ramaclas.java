3895c27b1df34611b32adb9f4ba57d3f6111f3ef
appca <appca@DESKTOP-IK5GG7B>
2025-03-26 18:01:14 +0100

    Primer Cambio rama

--- proleiregiovanna/src/paqueterama/ramaclas.java
+++ proleiregiovanna/src/paqueterama/ramaclas.java
@@ -5,6 +5,8 @@ public class ramaclas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("Esto es una rama local");
+  System.out.println("Esto es infinito");
+  
 	}
 
 }
