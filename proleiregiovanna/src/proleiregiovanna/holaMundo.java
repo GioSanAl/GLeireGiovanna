0c370dab3063da574660caaa6b8b3b1dd1eec187
appca <appca@DESKTOP-IK5GG7B>
2025-03-26 17:24:28 +0100

    Cmbio giovanna provocar conflicto

--- proleiregiovanna/src/proleiregiovanna/holaMundo.java
+++ proleiregiovanna/src/proleiregiovanna/holaMundo.java
@@ -4,6 +4,6 @@ public class holaMundo {
 
 	public static void main (String [] args) {
 		
-		System.out.println("Voy a por las niñas al cole!");
+		System.out.println("Voy a por las niñas al cole a llevarlas la merienda!");
 	}
 }
