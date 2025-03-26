c894caea11d5a05d9cf76531d51f685d99f99951
appca <appca@DESKTOP-IK5GG7B>
2025-03-26 17:55:51 +0100

    Rama Gio

---
+++ proleiregiovanna/src/paqueterama/ramaclas.java
@@ -0,0 +1,10 @@
+package paqueterama;
+
+public class ramaclas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Esto es una rama local");
+	}
+
+}
