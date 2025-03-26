31dccfd3d700ce81b9f823a12945ee9edbcfab33
leire <leire@Lenovo>
2025-03-26 17:30:05 +0100

    Cambio Leire

--- proleiregiovanna/src/proleiregiovanna/holaMundo.java
+++ proleiregiovanna/src/proleiregiovanna/holaMundo.java
@@ -4,6 +4,7 @@ public class holaMundo {
 
 	public static void main (String [] args) {
 		
-		System.out.println("Voy a por las niñas al cole!");
+		System.out.println("Voy a por las niï¿½as al cole!");
+		System.out.println("No voy");
 	}
 }
